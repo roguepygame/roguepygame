587231f13a97fdae4e3f65fcefdc1c72f535abb2
MightyJosip <josip.komljenovic@fer.hr>
2022-12-03 13:21:28 +0100

    Add documentation and type hints for project base

--- roguepygame/constants.py
+++ roguepygame/constants.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING, Optional
 if TYPE_CHECKING:
     from game import Game
 
-program: Optional[Game] = None  # Game class
+program: Optional["Game"] = None  # Game class
 
 WIDTH: int = 800
 HEIGHT: int = 600
