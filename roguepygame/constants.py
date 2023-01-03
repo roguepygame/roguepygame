ca582308b0277a6684c1396a93930c0bb13d5428
MightyJosip <josip.komljenovic@fer.hr>
2023-01-04 00:03:16 +0100

    Fixing some stuff No.2

--- roguepygame/constants.py
+++ roguepygame/constants.py
@@ -15,4 +15,5 @@ FPS: int = 60
 
 FONT_NAME: str = pygame.font.get_default_font()
 FOLDER = os.path.dirname(sys.modules['__main__'].__file__)
-IMAGE_FOLDER = os.path.join(FOLDER, 'assets\\images')
+ASSETS_FOLDER = os.path.join(FOLDER, 'assets')
+IMAGE_FOLDER = os.path.join(ASSETS_FOLDER, 'images')
